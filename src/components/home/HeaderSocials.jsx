a20cde2b285b7ec5f33488f4e1668765cbb04465
PerryPerrz <iophugstud6@gmail.com>
2023-07-25 08:46:45 +0200

    Adding the homepage

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+    return (
+        <div className="home-socials">
+            <a href="https://www.instagram.com/" className="home-social-link" target="_blank">
+                <i className="fa-brands fa-github"></i>
+            </a>
+
+            <a href="https://www.instagram.com/" className="home-social-link" target="_blank">
+                <i className="fa-brands fa-figma"></i>
+            </a>
+
+            <a href="https://www.instagram.com/" className="home-social-link" target="_blank">
+                <i className="fa-brands fa-twitter"></i>
+            </a>
+
+            <a href="https://www.instagram.com/" className="home-social-link" target="_blank">
+                <i className="fa-brands fa-dribbble"></i>
+            </a>
+
+            <a href="https://www.instagram.com/" className="home-social-link" target="_blank">
+                <i className="fa-brands fa-pinterest"></i>
+            </a>
+        </div>
+    )
+}
+
+export default HeaderSocials
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 
 import './Home.css';
 
+import Me from "../../assets/avatar-1.svg";
+
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
+import Shapes from './Shapes';
+
 function Home() {
   return (
-    <div>Home</div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home-img" />
+        <h1 className="home-name">Perry Perrz</h1>
+        <span className="home-education">I'm a Software Engineer</span>
+
+        <HeaderSocials />
+
+        <a href="#contact" className="btn">Hire Me</a>
+
+        <ScrollDown />
+      </div>
+      
+      <Shapes />
+    </section>
   )
 }
 
