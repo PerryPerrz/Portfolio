a6c855efc9c615064c611be360993586971d2edb
PerryPerrz <iophugstud6@gmail.com>
2023-08-02 15:14:36 +0200

    Adding technologies

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -5,42 +5,67 @@ import Work4 from "../../assets/work-4.svg";
 import Work5 from "../../assets/work-5.svg";
 import Work6 from "../../assets/work-6.svg";
 
+import Angular from "../../assets/logo/angular.svg";
+import C from "../../assets/logo/c.svg";
+import HTMLCSS from "../../assets/logo/html-css.svg";
+import Java from "../../assets/logo/java.svg";
+import JavaScript from "../../assets/logo/js.svg";
+import LibGDX from "../../assets/logo/libgdx.svg";
+import MySQL from "../../assets/logo/mysql.svg";
+import NodeJS from "../../assets/logo/nodejs.svg";
+import Numpy from "../../assets/logo/numpy.svg";
+import PHP from "../../assets/logo/php.svg";
+import Python from "../../assets/logo/python.svg";
+import React from "../../assets/logo/react.svg";
+import TypeScript from "../../assets/logo/typescript.svg";
+
 const Menu = [
     {
         id: 1,
         image: Work1,
         title: "Project Management Illustration",
         category: "Design",
+        technology1: Angular,
     },
     {
         id: 2,
         image: Work2,
         title: "Guest App Walkthrough Screens",
         category: "Art",
+        technology1: C,
+        technology2: HTMLCSS,
     },
     {
         id: 3,
         image: Work3,
         title: "Delivery App Wireframe",
         category: "Branding",
+        technology1: Java,
     },
     {
         id: 4,
         image: Work4,
         title: "Onboarding Motivation",
         category: "Design",
+        technology1: JavaScript,
+        technology2: LibGDX,
+        technology3: MySQL,
     },
     {
         id: 5,
         image: Work5,
         title: "iMac Mockup Design",
         category: "Creative",
+        technology1: NodeJS,
     },
     {
         id: 6,
         image: Work6,
         title: "Game Store App Concept",
         category: "Art",
+        technology1: "",
+        technology2: Numpy,
+        technology3: "",
     },
 ];
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -14,6 +14,13 @@ function Portfolio() {
         setItems(updatedItems);
     }
 
+    const renderTechnologyImage = (technology) => {
+        if (technology && typeof technology === 'string' && technology.trim() !== "") {
+            return <img src={technology} alt="" className="technology" />;
+        }
+        return null;
+    };
+
     return (
         <section className="work container section" id="portfolio">
             <h2 className="section-title">Recent Works</h2>
@@ -28,12 +35,18 @@ function Portfolio() {
 
             <div className="work-container grid">
                 {items.map((elem) => {
-                    const { id, image, title, category } = elem;
+                    const { id, image, title, category, technology1, technology2, technology3 } = elem;
                     return (
                         <div className="work-card" key={id}>
                             <div className="work-thumbnail">
                                 <img src={image} alt="" className="work-img" />
-                                <div className="work-mask"></div>
+                                <div className="work-mask">
+                                    <div className="technologies-container">
+                                        {renderTechnologyImage(technology1)}
+                                        {renderTechnologyImage(technology2)}
+                                        {renderTechnologyImage(technology3)}
+                                    </div>
+                                </div>
                             </div>
 
                             <span className="work-category">{category}</span>
