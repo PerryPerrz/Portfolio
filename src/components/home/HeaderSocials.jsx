4715b2def6a4f3b7c61844eeabd79154730f1617
PerryPerrz <iophugstud6@gmail.com>
2023-08-10 10:28:33 +0200

    Adding links

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 const HeaderSocials = () => {
     return (
         <div className="home-socials">
-            <a href="https://github.com/PerryPerrz" className="home-social-link" target="_blank">
+            <a href="https://github.com/PerryPerrz/" className="home-social-link" target="_blank">
                 <i className="fa-brands fa-github"></i>
             </a>
 
-            <a href="https://www.instagram.com/" className="home-social-link" target="_blank">
+            <a href="https://www.figma.com/" className="home-social-link" target="_blank">
                 <i className="fa-brands fa-figma"></i>
             </a>
 
-            <a href="https://www.instagram.com/" className="home-social-link" target="_blank">
+            <a href="https://twitter.com/" className="home-social-link" target="_blank">
                 <i className="fa-brands fa-twitter"></i>
             </a>
 
-            <a href="https://www.instagram.com/" className="home-social-link" target="_blank">
+            <a href="https://dribbble.com/shots/" className="home-social-link" target="_blank">
                 <i className="fa-brands fa-dribbble"></i>
             </a>
 
-            <a href="https://www.instagram.com/" className="home-social-link" target="_blank">
-                <i className="fa-brands fa-pinterest"></i>
+            <a href="https://www.behance.net/" className="home-social-link" target="_blank">
+                <i className="fa-brands fa-behance"></i>
             </a>
         </div>
     )
