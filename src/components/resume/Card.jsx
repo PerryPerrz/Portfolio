a09f644e529be8669bea1a6d8f0c21ca6f92d398
PerryPerrz <iophugstud6@gmail.com>
2023-09-10 17:57:26 +0200

    Add translations

--- src/components/resume/Card.jsx
+++ src/components/resume/Card.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
+import { useTranslation } from 'react-i18next';
+
 const Card = (props) => {
+    const [t, i18n] = useTranslation("global");
     return (
         <div className="timeline-item">
             <i className={props.icon}></i>
-            <span className="timeline-date">{props.year}</span>
-            <h3 className="timeline-title">{props.title}</h3>
-            <p className="timeline-text">{props.desc}</p>
+            <span className="timeline-date">{t(`${props.category}`.year)} {props.year}</span>
+            <h3 className="timeline-title">{t(`${props.category}.${props.title}`)}</h3>
+            <p className="timeline-text">{t(`${props.category}.${props.desc}`)}</p>
         </div>
     )
 }
