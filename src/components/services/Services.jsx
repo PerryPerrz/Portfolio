a54ae81e2e69877c0a4e8f3bb0fdc318a6b50f7e
PerryPerrz <iophugstud6@gmail.com>
2023-09-10 09:20:01 +0200

    Add translations

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,38 +2,42 @@ import React from 'react';
 
 import './Services.css';
 
+import { useTranslation } from 'react-i18next';
+
 import Image1 from '../../assets/service-1.svg';
 import Image2 from '../../assets/service-2.svg';
 import Image3 from '../../assets/service-3.svg';
 
-const data = [
-    {
-        id: 1,
-        image: Image1,
-        title: "Web Development",
-        description:
-            "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
-    },
-    {
-        id: 2,
-        image: Image2,
-        title: "UI/UX Design",
-        description:
-            "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
-    },
-    {
-        id: 3,
-        image: Image3,
-        title: "Photography",
-        description:
-            "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
-    },
-];
-
 function Services() {
+    const [t, i18n] = useTranslation("global");
+
+    const data = [
+        {
+            id: 1,
+            image: Image1,
+            title: t("services.service-1"),
+            description:
+                t("services.description-1"),
+        },
+        {
+            id: 2,
+            image: Image2,
+            title: t("services.service-2"),
+            description:
+                t("services.description-2"),
+        },
+        {
+            id: 3,
+            image: Image3,
+            title: t("services.service-3"),
+            description:
+                t("services.description-3"),
+        },
+    ];
+
     return (
         <section className="services container section" id="services">
-            <h2 className="section-title">Services</h2>
+            <h2 className="section-title">{t("services.title")}</h2>
 
             <div className="services-container grid">
                 {data.map(({ id, image, title, description }) => {
