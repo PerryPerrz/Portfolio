362fae3eb9f6c047ce513861c38f3fddca321020
PerryPerrz <iophugstud6@gmail.com>
2023-09-16 20:46:10 +0200

    Updating graphic content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './About.css';
 
-import Image from "../../assets/avatar-1.png";
+import Image from "../../assets/avatar-2.png";
 import AboutBox from './AboutBox';
 
 import { useTranslation } from 'react-i18next';
